b2bd97d566d58339dbf8e52b923bd00d99d5bcf3
huteko <htooko1101@gmail.com>
2018-06-27 17:50:46 +0630

    Val vs Var

---
+++ src/One.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+
+    // val vs var
+    // val is immutable
+    // var is mutable
+
+    // assign "Marg" string to immutable variable called name
+    val name: String = "Marg"
+    // same as val name: String = "Marg"
+    val nameTwo = "Marg"
+    // this is not working
+    //name = "Gate"
+
+    // created object nameThree as mutable
+    var nameThree: String
+    // assign "Stella" to nameThree
+    nameThree = "Stella"
+
+
+}
