22577965b15a7a8e881e5da75f956a1df8c73685
huteko <htooko1101@gmail.com>
2018-06-27 18:05:10 +0630

    Variables

---
+++ src/Two.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+
+    // variable
+    val isAlive: Boolean = true
+    val myByte: Byte = -127
+    val myChar: Char = 'H'
+    val myShort: Short = 30
+    val myInt: Int = 1
+    val myLong: Long = -2L
+    val myFloat: Float = 4.38F
+    val myDouble: Double = 43.88
+
+    // Size
+//    Boolean   1bit
+//    Byte      8bit
+//    Char      16bit
+//    Short     16bit
+//    Int       32bit
+//    Long      64bit
+//    Float     32bit
+//    Double    64bit
+
+}
